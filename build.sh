3cc1ac2d60c8f3aa14d4f03dc3aad265755371c7
Sakuragawa Misty <gyc990326@gmail.com>
2021-11-22 20:34:14 +0800

    Change to use https:// instead of git://

--- build.sh
+++ build.sh
@@ -1,27 +1,30 @@
 #!/usr/bin/bash
 SCRIPT=$(realpath $0)
 ORIG=$(dirname $SCRIPT)
-DEST=$(realpath .)
+DEST="$(realpath .)"
+PLATFORM="efi"
+TARGET="x86_64"
 cd $DEST
 #apt-get source grub-pc
-if [[ ! -d $DEST/grub ]]; then
+if [ ! -d "$DEST/grub" ]; then
     echo "Downloading grub source code..."
-    git clone git://git.savannah.gnu.org/grub.git
+    git clone https://git.savannah.gnu.org/git/grub.git
     cd grub
     
     echo "Building..."
     cp $ORIG/*.c ./grub-core/commands/
-    cp $ORIG/*.h ./include/grub/
+
     if [ "$(cat ./grub-core/Makefile.core.def | grep vmware | wc -l)" -eq "0" ]; then
         cat $ORIG/Makefile.core.def >> ./grub-core/Makefile.core.def
     fi
 
+    export GNULIB_URL="https://git.savannah.gnu.org/git/gnulib.git"
     ./bootstrap
     ./linguas.sh
     echo "es" > po/LINGUAS
     ./autogen.sh
     # http://www.linuxfromscratch.org/lfs/view/development/chapter06/grub.html
-    ./configure --prefix=/usr --sbindir=/sbin --sysconfdir=/etc --disable-werror
+    ./configure --with-platform=$PLATFORM --target=$TARGET --prefix=/usr --sbindir=/sbin --sysconfdir=/etc --disable-werror
     cd ..
 fi
 
